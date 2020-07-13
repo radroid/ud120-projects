96f00e5a96447a621efef3bd7b3ff1e004eedccc
Raj Dholakia <raj9dholakia@gmail.com>
2020-07-13 18:22:44 +0530

    Naive Bayes Mini Project - Add required Modules - Download and add email data - Update nb_author_id.py  * Train training data  * Test the model with testing data  * Calculate and print accuracy  * Calculate and print time to train and test data

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,27 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
-### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
 
-
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training time:", round(time() - t0, 3), "s"
+t0 = time()
+accuracy = clf.score(features_test, labels_test)
+print "Measuring Accuracy:", round(time() - t0, 3), "s"
+print("Accuracy: {}".format(round(accuracy, 3)))
 #########################################################
-
-
